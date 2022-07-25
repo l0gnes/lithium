219f913971658cb367367de3b291cee43c9af3cf
lognes <97571297+l0gnes@users.noreply.github.com>
2022-07-25 02:48:01 -0300

    changes to cog infrastructure

--- core/Core.py
+++ core/Core.py
@@ -3,10 +3,23 @@ from discord import app_commands
 from discord.ext import commands
 from .db.hook import CoreDatabaseHook
 from typing import List
+from utils.LithiumCog import LithiumCog
 
-class Core(commands.Cog):
+class Core(LithiumCog):
+
+    ###
 
     db_hook : CoreDatabaseHook
+    db_hook_class = CoreDatabaseHook
+
+    cog_id = "core"
+    name = "Core"
+    author = "l0gnes"
+    version = 1.0
+
+    requires_database = True
+
+    ###
 
     whitelist_commands = app_commands.Group(
         name = "whitelist",
@@ -15,16 +28,6 @@ class Core(commands.Cog):
 
     def __init__(self, client : commands.Bot):
         self.client = client
-
-    async def cog_load(self):
-        try:
-            await self.client.db.set_hook(
-                "core", CoreDatabaseHook
-            )
-        except Exception as err:
-            raise err
-        else:
-            self.db_hook = self.client.db.get_hook("core")
 
     @app_commands.command(name="ping", description="Check to see if the bot is up!")
     async def core_ping(self, interaction : discord.Interaction) -> None:
---
+++ utils/LithiumCog.py
@@ -0,0 +1,42 @@
+from discord.ext import commands
+from typing import List, Optional, Type, Union
+from database import DatabaseHook
+
+class LithiumCog(commands.Cog):
+
+    requires : List[str] = []
+    requires_database : bool = True
+
+    db_hook : Optional[DatabaseHook] = None
+    db_hook_class : Optional[Type[DatabaseHook]] = None
+    cog_id: str # This must be unique amongst all of the LithiumCogs, this is also used for db hook keys
+    
+    name : Optional[str]
+    author : Optional[str]
+    version : Optional[Union[str, float]]
+    
+    async def cog_load(self):
+
+        if self.requires_database and self.db_hook_class is not None:
+
+            try:
+                await self.client.db.set_hook(
+                    self.cog_id, self.db_hook_class
+                )
+            except Exception as err:
+                raise err
+            else:
+                self.db_hook_class = self.client.db.get_hook(self.cog_id)
+
+        print(f"Cog: {self.name} {self.version} by {self.author} has been loaded")
+
+        return await super().cog_load()
+
+    async def lithium_cog_after_load(self):
+        # Checks to make sure that everything that is required to be loaded actually is
+        # We do this after because I'm too lazy to do checking before loading the cogs.
+        # This is probably bad since you shouldn't have to unload a cog that shouldn't
+        # have been loaded in the first place but oh well lmao
+
+        # TODO: Check to make sure that "requires" has it's requirements met.
+        return False
